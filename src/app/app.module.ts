9003425d3ef311c6b4d0a63620c7d9794393bc1e
Michael Barcellona <michaelbarce@gmail.com>
2018-08-30 13:28:13 -0400

    Added Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListDetailComponent } from './list-detail/list-detail.component';
 import { ContactComponent } from './contact/contact.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DataService } from './service/data.service';
-
+import { RouterModule, CanActivate } from '@angular/router';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, 
   MatListModule, MatCheckboxModule, MatGridListModule, 
   MatCardModule, MatTableModule, MatFormFieldModule,
   MatInputModule, MatProgressSpinnerModule, MatTabsModule, 
   MatOptionModule, MatSelectModule, MatChipsModule, MatSlideToggleModule } from '@angular/material';
+import { MapBoxComponent } from './map-box/map-box.component';
+import { ROUTES } from './app.routes';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ListDetailComponent,
-    ContactComponent
+    ContactComponent,
+    MapBoxComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule
+     BrowserModule
+    ,BrowserAnimationsModule
+    ,HttpClientModule
+    ,RouterModule.forRoot(ROUTES)
     ,MatToolbarModule, MatButtonModule, MatSidenavModule
     ,MatIconModule, MatListModule, MatCheckboxModule, MatGridListModule
     ,MatCardModule, MatTableModule, MatFormFieldModule, MatInputModule
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  
+  production: false,
+
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoibWljaGFlbGJhcmNlIiwiYSI6ImNqbGduMmE3YTE5NXkza21pM2JxYzc4MGoifQ.LxtcLitsck9u4yE6xZBPVQ' 
+  }
+
 };
 
 /*
